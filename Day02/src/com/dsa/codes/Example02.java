33a5f724e330adbb00b1b99e971103d76bc129db
Vedx23 <paturkarvedant@gmail.com>
2025-07-30 10:53:45 +0530

    feat : added bubble sort

--- Day02/src/com/dsa/codes/Example02.java
+++ Day02/src/com/dsa/codes/Example02.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class Example02 {
 
-    public static int[] selectionSort(int[] arr){
-
+    public static void selectionSort(int[] arr){
+        long start = System.nanoTime();
         int size = arr.length;
 
         for (int i = 0; i < size - 1; i++){
@@ -18,13 +18,70 @@ public class Example02 {
             }
         }
 
-        return arr;
+        long end = System.nanoTime();
+
+        System.out.println("Time Taken - " + (end - start) + "ns");
+    }
+
+    public static void bubbleSort(int[] arr){
+
+        long start = System.nanoTime();
+
+        int size = arr.length;
+
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size - 1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        long end = System.nanoTime();
+
+        System.out.println("Time taken - " + (end-start) + "ns");
+
+    }
+
+    public static void improvedBubbleSort(int[] arr){
+
+        long start = System.nanoTime();
+
+        int size = arr.length;
+
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size - i - 1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        long end = System.nanoTime();
+
+        System.out.println("Time taken - " + (end-start) + "ns");
+
     }
 
     public static void main(String[] args) {
         int[] arr = {2,4,62,1,46,89,0,0,-1};
         System.out.println(Arrays.toString(arr));
-        int[] sorted = selectionSort(arr);
+
+        selectionSort(arr);
+
         System.out.println(Arrays.toString(arr));
+
+        bubbleSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+
+        improvedBubbleSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+
     }
 }
